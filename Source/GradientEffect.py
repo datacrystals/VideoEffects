14079e15f3a6f0c7980c294c434d40385d941b2c
datacrystals <tliao@carboncopies.org>
2022-10-03 01:23:46 +0000

    Fix Trip

--- Source/GradientEffect.py
+++ Source/GradientEffect.py
@@ -89,23 +89,27 @@ def WriteFrames(Path:str, Frames:list, VideoProperties:FrameInfo):
     Log("Releasing VideoWriter")
     Writer.release()
 
-def sp_noise(image,prob):
+def sp_noise(image, prob):
     '''
     Add salt and pepper noise to image
     prob: Probability of the noise
     '''
-    output = numpy.zeros(image.shape,numpy.uint8)
-    thres = 1 - prob 
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            rdn = random.random()
-            if rdn < prob:
-                output[i][j] = 0
-            elif rdn > thres:
-                output[i][j] = 255
-            else:
-                output[i][j] = image[i][j]
-    return output
+    output = image.copy()
+    if len(image.shape) == 2:
+        black = 0
+        white = 255            
+    else:
+        colorspace = image.shape[2]
+        if colorspace == 3:  # RGB
+            black = np.array([0, 0, 0], dtype='uint8')
+            white = np.array([255, 255, 255], dtype='uint8')
+        else:  # RGBA
+            black = np.array([0, 0, 0, 255], dtype='uint8')
+            white = np.array([255, 255, 255, 255], dtype='uint8')
+    probs = np.random.random(image.shape[:2])
+    image[probs < (prob / 2)] = black
+    image[probs > 1 - (prob / 2)] = white
+    return image
 
 # Main Processing Function
 def ProcessFrames(Frames:list, Arguments:list):
@@ -125,7 +129,7 @@ def ProcessFrames(Frames:list, Arguments:list):
         Frame = Frame / Amount
         Frame = Frame * Amount
 
-        Frame = sp_noise(Frame, 0.01*Amount)
+        Frame = sp_noise(Frame, 0.01)
 
         Frames[FrameIndex] = Frame
         Log(f"Processed Frame [{FrameIndex+1}/{NumberFrames}] ({round((FrameIndex+1)*100/NumberFrames)}%)")
